e0eed32240267b0e7a3c8e6fc4f0aebd996427cb
phpdevonj <phpdevelopment@ongraph.com>
2023-05-30 15:21:35 +0530

    Validations revised

--- app/Http/Requests/UpsertLeadRequest.php
+++ app/Http/Requests/UpsertLeadRequest.php
@@ -28,12 +28,26 @@ class UpsertLeadRequest extends FormRequest
         return [
             'first_name' => 'required',
             'last_name' => 'required',
-            'email' => 'required|email',
-            'mobile' => ['required','regex:/^[0-9]{10}+$/','numeric','min:10'],
-            'dob' => 'required',
-            'tax_file_number' => ['required','min:9'],
-            'agreed_terms' => 'required',
-            'status' => 'required'
+            'email' => ['required', 'email'],
+            'mobile' => ['required', 'regex:/^04\d{2} \d{3} \d{3}$/'],
+            'dob' => ['required', 'date_format:Y-m-d'],
+            'tax_file_number' => ['required','digits:9'],
+            'agreed_terms' => ['required', 'in:Yes,No'],
+            'status' => ['required', 'in:Ready For Search,New Prospect']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'first_name.required' => 'Prospect\'s First Name is Required',
+            'last_name.required' => 'Prospect\'s Last Name is Required',
+            'mobile.required' => 'The mobile number is required.',
+            'mobile.regex' => 'Mobile Number must be of the Australian mobile number format (I,e, 04XX XXX XXX)',
+            'dob.date_format' => 'DOB must be of the format YYYY-MM-DD, I.e. 1984-12-23',
+            'tax_file_number.digits' => 'Tax File Number must have 9 digits with no spaces',
+            'agreed_terms.in' => 'Agreed terms must be either Yes or No',
+            'status.in' => 'Status must be either Ready For Search or New Prospect'
         ];
     }
 
